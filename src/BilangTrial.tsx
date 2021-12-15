8b80086eb6cc8bfa0ecd1cab967770a12872dd10
Eugene Gorbachev <eugeneg@cqg.com>
2021-12-15 10:53:51 +0300

    Displaying 2 sentences table with 3 mode button

---
+++ src/BilangTrial.tsx
@@ -0,0 +1,135 @@
+import { useState, useEffect } from "react"
+//import queryString from 'query-string';
+import { dataDb, DeckData, RowData } from "./DataDB"
+
+enum TrialMode {
+  Translate,
+  Review,
+  Enter
+}
+
+interface HeaderProps {
+  title: string
+}
+
+const Header =({title}: HeaderProps) => 
+<>
+  <h1>{title}</h1>
+</>
+
+interface RowProps {
+  currentMode: TrialMode;
+  textInLanguage1: string;
+  textInLanguage2: string;
+}
+
+const Row = ( {currentMode, textInLanguage1, textInLanguage2}: RowProps) => {
+  switch(currentMode)
+  {
+    case TrialMode.Translate: 
+      const text2 = '\u00A0'.repeat(7)
+      return <tr>
+              <td>{textInLanguage1}</td>
+              <td>{text2}</td>
+            </tr>
+    case TrialMode.Review:
+      return <tr>
+              <td>{textInLanguage1}</td>
+              <td>{textInLanguage2}</td>
+            </tr>
+    case TrialMode.Enter:
+      return <tr>
+              <td><input></input></td>
+              <td>{textInLanguage2}</td>
+            </tr>
+    default:
+      return <></>
+  }
+
+}
+
+interface SectionProps {
+  currentMode: TrialMode;
+  onModeChange: (mode: TrialMode) => void;
+  deck: DeckData
+}
+
+const Section = ({currentMode, onModeChange, deck} : SectionProps) => {
+  const rows = deck.rows.map((value: RowData, index) => {
+    return <Row  currentMode={currentMode}
+                 textInLanguage1={value.sentence1}
+                 textInLanguage2={value.sentence2}
+                 key={index}/>
+  }) 
+
+  return (
+    <>
+      <div>
+        <button className={'btn btn-primary m-2 ' + (currentMode===TrialMode.Translate ? "disabled" : "")}
+            onClick={ ()=> onModeChange(TrialMode.Translate)} >
+              Translate</button>
+        <button className={'btn btn-primary m-2 ' + (currentMode===TrialMode.Review? "disabled" : "")}
+            onClick={ ()=> onModeChange(TrialMode.Review)} >
+              Review</button>
+        <button className={'btn btn-primary m-2 ' + (currentMode===TrialMode.Enter ? "disabled" : "")}
+        onClick={ ()=> onModeChange(TrialMode.Enter)} >
+              Enter</button>
+      </div>
+      <table className="table border thead-dark">
+        <caption>Chapter 1</caption>
+        <thead>
+          <tr><th>Language 1</th><th>Language 2</th></tr>
+        </thead>
+        <tbody>
+          {rows}
+        </tbody>
+      </table>
+    </>)
+}
+
+const Page = (props: any) => {
+  const nullDeck = {rows:[], id: 0, name: '', language: '', description: ''} as DeckData
+  const [deck, setDeck] = useState(nullDeck);
+  const [mode, setMode] = useState(TrialMode.Translate);
+
+  useEffect(() => {
+    if(deck?.name === nullDeck.name){
+       deckGetter();
+    }
+ });
+ 
+ /*
+ const urlParams = queryString.parse(props.location.search);
+ if (urlParams.id === undefined) {
+    //return null;
+ } else {
+
+ }
+ */
+/*
+ const deckId = urlParams.id;
+ */
+ const deckGetter = async () => {
+    const myDeckId = 1000
+
+    const deckData = await dataDb.getDeck(process.env.PUBLIC_URL, myDeckId)
+    if (deckData !== null)
+      setDeck(deckData);
+ }
+ 
+ if (deck === undefined)
+    return (<><h1 className='text-center'>Не такой колоды</h1></>)
+ 
+ document.documentElement.lang = deck.language
+
+  return (
+    <>
+      <div className='container-md'>
+        <Header title={deck.name}/>
+        <Section currentMode={mode} onModeChange={setMode} deck={deck}/>
+      </div>
+    </>
+  )
+}
+
+export {Page as BilangTrialPage}
---
+++ src/DataDB.ts
@@ -0,0 +1,143 @@
+import TextDecks from './deck/TextDecks.json' 
+
+interface DeckJsonRecord {
+    id : number,
+    name: string,
+    description: string,
+    language: string,
+    fileName: string
+}
+
+interface DeckInfo {
+    id : number,
+    name: string,
+    description: string,
+    language: string
+}
+
+interface RowData {
+    id: number,
+    //score:
+    sentence1: string,
+    sentence2: string
+}
+
+interface DeckData extends DeckInfo {
+    rows: RowData[]
+}
+
+async function* makeTextFileLineIterator(fileURL: string) {
+    const utf8Decoder = new TextDecoder('utf-8');
+    const response = await fetch(fileURL);
+    if(response === null)
+        return null
+    const body = response.body
+    if(body === null)
+        return null
+    const reader = body.getReader();
+    let { value: chunk, done: readerDone } = await reader.read();
+    let txtChunk = chunk ? utf8Decoder.decode(chunk) : '';
+  
+    const re = /\n|\r|\r\n/gm;
+    let startIndex = 0;
+  
+    for (;;) {
+      let result = re.exec(txtChunk);
+      if (!result) {
+        if (readerDone) {
+          break;
+        }
+        let remainder = txtChunk.substr(startIndex);
+        ({ value: chunk, done: readerDone } = await reader.read());
+        txtChunk = remainder + (chunk ? utf8Decoder.decode(chunk) : '');
+        startIndex = re.lastIndex = 0;
+        continue;
+      }
+      yield txtChunk.substring(startIndex, result.index);
+      startIndex = re.lastIndex;
+    }
+    if (startIndex < txtChunk.length) {
+      // last line didn't end in a newline char
+      yield txtChunk.substr(startIndex);
+    }
+  }
+
+     
+  async function getTextDeck(root_url: string, deckInfo: DeckJsonRecord) {
+      //
+    const url = `${root_url}/deck/${deckInfo.fileName}`
+    console.log(`-------------- ${url}`)
+    const rows = []
+    let index = 0
+    for await (let line of makeTextFileLineIterator(url)) {
+      if (line.length === 0 )
+        continue
+      const parts = line.split('\t')
+      const sentenceId = deckInfo.id * 10000 + index //getWordId(deckDesc.Id, index)
+      const newRecord = {
+        id: sentenceId,
+      //  score: 0, // getWordScore(wordId)
+        sentence1: '',
+        sentence2: ''
+      } as RowData
+
+      switch (parts.length) {
+        case 1:
+          newRecord.sentence1 = parts[0]
+          break
+        case 2:
+          newRecord.sentence1 = parts[0]
+          newRecord.sentence2 = parts[1]
+          break
+    /*    case 3:
+          newRecord.word = parts[0]
+          newRecord.extra = parts[1] ? parts[1]: undefined
+          newRecord.meaning = parts[2]
+          break */
+        default:
+          break
+      }
+
+      rows.push(newRecord)
+      ++index
+    }
+    return {
+      id: deckInfo.id,
+      name: deckInfo.name,
+      description: deckInfo.description,
+      language: deckInfo.language,
+      rows: rows
+    } as DeckData
+  }
+
+class DataDB {
+    decks: DeckJsonRecord[]
+
+    constructor() {
+        this.decks = TextDecks
+    }
+
+    getAllDecks() : DeckInfo[] {
+        return this.decks.map((value: DeckJsonRecord, index: number) => {
+            return { 
+                id: value.id, 
+                name: value.name,
+                description: value.description,
+                language: value.language
+             } as DeckInfo
+        })
+    }
+
+    async getDeck(root_url: string, deckId: number): Promise<DeckData|null> {
+        const found = this.decks.find((value: DeckJsonRecord) => value.id === deckId)
+        if (found !== undefined) {
+            return getTextDeck(root_url, found)
+        }
+        return null
+    }
+}
+
+const dataDb = new DataDB()
+
+export { dataDb }
+export type { DeckData, RowData }
